5c6e6eafee409689f98bb38a2e87061fb6de40c5
trongquochuydinh <trongquochuydinh@gmail.com>
2025-02-12 12:34:03 +0100

    Adjusted colors for light theme

--- testing/app/src/main/java/com/example/testing/ui/theme/Color.kt
+++ testing/app/src/main/java/com/example/testing/ui/theme/Color.kt
@@ -6,6 +6,6 @@ val BackgroundAppDarkThemeColor = Color(0xFF625A5A)
 val BackgroundButtonDarkThemeColor = Color(0xFFD9D9D9)
 val IconDarkThemeColor = Color(0xFF1E1E1E)
 
-val BackgroundAppLightThemeColor = Color(0xFFD9D9D9)
-val BackgroundButtonLightThemeColor = Color(0xFF625A5A)
+val BackgroundAppLightThemeColor = Color(0xFFF3EDF7)
+val BackgroundButtonLightThemeColor = Color(0xFFD9D9D9)
 val IconDarkLightColor = Color(0xFF1E1E1E)
